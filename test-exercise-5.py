7f2328d550c327af1db68c0d72a8875be4d9f6f2
Chanveasna-ENG <137870404+Chanveasna-ENG@users.noreply.github.com>
2023-07-29 14:25:44 +0700

    Update test-exercise-5.py

--- test-exercise-5.py
+++ test-exercise-5.py
@@ -18,14 +18,9 @@ class CodingRoomsUnitTests(unittest.TestCase):
         self.assertTrue(True)
 
     def test_reverse_ascending(self):
-
         self.assertEqual(reverse_ascending([1, 2, 3, 4, 5]),  [5, 4, 3, 2, 1])
-
-        self.assertEqual(reverse_ascending([5, 7, 10, 4, 2, 7, 8, 1, 3]),  [
-            10, 7, 5, 4, 8, 7, 2, 3, 1])
-
+        self.assertEqual(reverse_ascending([5, 7, 10, 4, 2, 7, 8, 1, 3]),  [10, 7, 5, 4, 8, 7, 2, 3, 1])
         self.assertEqual(reverse_ascending([5, 4, 3, 2, 1]),  [5, 4, 3, 2, 1])
-
         self.assertEqual(reverse_ascending([]),  [])
 
 

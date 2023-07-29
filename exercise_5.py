ec985794b34fedae98ce980521364fa59938d02c
Chanveasna-ENG <137870404+Chanveasna-ENG@users.noreply.github.com>
2023-07-29 17:47:01 +0700

    Submit exercise 5

--- exercise_5.py
+++ exercise_5.py
@@ -1,2 +1,26 @@
 def reverse_ascending(numbers):
-    ...
+    result = []
+    ascending_sequence = []
+
+    for index, current_value in enumerate(numbers):
+        if index - 1 < 0:
+            continue
+
+        if current_value > numbers[index - 1]:
+            if ascending_sequence:
+                ascending_sequence.append(current_value)
+            else:
+                ascending_sequence.extend([numbers[index - 1], current_value])
+        else:
+            if ascending_sequence:
+                result.extend(ascending_sequence[::-1])
+                ascending_sequence.clear()
+            else:
+                result.append(numbers[index - 1])
+
+    if ascending_sequence:
+        result.extend(ascending_sequence[::-1])
+    if len(result) != len(numbers):
+        result.append(numbers[-1])
+
+    return result
--- test-exercise-5.py
+++ test-exercise-5.py
@@ -21,6 +21,11 @@ class CodingRoomsUnitTests(unittest.TestCase):
         self.assertEqual(reverse_ascending([1, 2, 3, 4, 5]),  [5, 4, 3, 2, 1])
         self.assertEqual(reverse_ascending([5, 7, 10, 4, 2, 7, 8, 1, 3]),  [10, 7, 5, 4, 8, 7, 2, 3, 1])
         self.assertEqual(reverse_ascending([5, 4, 3, 2, 1]),  [5, 4, 3, 2, 1])
+        self.assertEqual(reverse_ascending([1, 2, 1, 2, 1, 2, 1, 2]),  [2, 1, 2, 1, 2, 1, 2, 1])
+        self.assertEqual(reverse_ascending([1,1,1,1,1,1,1,1,2]),  [1,1,1,1,1,1,1,2,1])
+        self.assertEqual(reverse_ascending([1,1,1,1,1,1,1,1,2,2,2,2,2,2]),  [1,1,1,1,1,1,1,2,1,2,2,2,2,2])
+        self.assertEqual(reverse_ascending([1,2]),  [2,1])
+        self.assertEqual(reverse_ascending([1]),  [1])
         self.assertEqual(reverse_ascending([]),  [])
 
 

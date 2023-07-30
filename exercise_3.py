76659ddae78334a7d00befafde365720211396e2
Holy-yth <137748095+Holy-yth@users.noreply.github.com>
2023-07-30 18:07:27 +0700

    done (#3)

--- exercise_3.py
+++ exercise_3.py
@@ -1,2 +1,8 @@
 def remove_all_after(numbers, n):
-    ...
+    result_list = []
+    for number in numbers:
+        if number == n:
+            result_list.append(number)
+            break
+        result_list.append(number)
+    return result_list
